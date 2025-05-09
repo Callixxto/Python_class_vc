0fe3a13b6f7e5c286949699e40318fe9cc4b7d55
vdi-terminal <san.stolarek@gmail.com>
2025-05-09 09:05:20 +0200

    Revert "Update conv.py"
    This reverts commit fdfe7fdb1a77760fd0757c085a7e48c46065420d.

--- conv.py
+++ conv.py
@@ -1,12 +1,3 @@
 name = prit(input(" Hello, what is your name? "))
 
 print(f"Hello {name}, it's so nice to meet you (-w^)~~ !")
-
-
-def division():
-  a = 20
-  b = 10
-  print(a/b)
-
-
-division()
